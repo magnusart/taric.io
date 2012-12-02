3d202a26208139c684a9ee261f946eeee1bbf075
Magnus Andersson <magnus.andersson@solvies.se>
2012-12-03 00:35:59 +0100

    First crude version where gzip.pgp-files are read.

--- app/models/Taric.scala
+++ app/models/Taric.scala
@@ -5,6 +5,9 @@ import java.io.InputStream
 import scalax.io._
 import scalax.io.JavaConverters._
 import scalax.io.managed.InputStreamResource
+import org.bouncycastle.openpgp.PGPObjectFactory
+import org.bouncycastle.openpgp.PGPCompressedData
+import org.bouncycastle.openpgp.PGPLiteralData
 
 // Import the session management, including the implicit threadLocalSession
 import org.scalaquery.session._
@@ -22,47 +25,67 @@ import org.scalaquery.ql.extended.{ExtendedTable => Table}
 
 case class TaricCode(fullCode: String, startDate:String, endDate:Option[String]) {
   require("""(^\d{10}$|^\d{8}$)""".r.findFirstIn(fullCode).isDefined)
-
-  val ProdCodeMatch = """(\d\d\d\d)(\d\d)(\d\d)(\d\d)?""".r
-  val ProdCodeMatch(hs, hsSub, cn, pres) = fullCode // Extract code parts
+  val hs = fullCode.take(4)
+  val hsSub = fullCode.drop(4).take(2)
+  val cn = fullCode.drop(6).take(2)
+  val pres = if (fullCode.length == 10) Some(fullCode.drop(8).take(2)) else None
 }
 
-case class ProductCode(id:Int, hs:String, hsSub:String, pres:Option[String],
-                       active:Boolean, ptype:String, startDate:String)
+case class ProductCode(id:Int, hs:String, hsSub:String, cn:String, pres:Option[String],
+    active:Boolean, ptype:String, startDate:String)
 
-object ProductCodes extends Table[(Int, String, String, String, String, Boolean, String, String)]("PRODUCT_CODES") {
-  def id = column[Int]("PRDC_ID", O.PrimaryKey)
+object ProductCodes extends Table[(Int, String, String, String, Option[String],
+                                  Boolean, String, String)]("PRODUCT_CODES") {
+  def id = column[Int]("PRDC_ID", O AutoInc)
   def hs = column[String]("PRDC_HS")
   def hsSub = column[String]("PRDC_HSSUB")
   def cn = column[String]("PRDC_CN")
-  def pres = column[String]("PRDC_PRES")
+  def pres = column[Option[String]]("PRDC_PRES")
   def active = column[Boolean]("PRDC_ACTIVE")
   def ptype = column[String]("PRDC_TYPE")
   def startDate = column[String]("PRDC_DATE")
   def * = id ~ hs ~ hsSub ~ cn ~ pres ~ active ~ ptype ~ startDate
+
+  val insertProjection = hs ~ hsSub ~ cn ~ pres ~ active ~ ptype ~ startDate
 }
 
 object TaricProdCodeParser {
-  def prodcode(line:String) = line.drop(3).take(10).trim
-  def startDate(line:String) = line.drop(13).take(8).trim
-  def endDate(line:String) = {
-    val tmp = line.drop(21).take(8).trim
-    if(tmp.length > 0) Option(tmp)
-    else None
-  }
-
-  def parseFromURL(url:String) = {
+  def parseFromURL(url:String):LongTraversable[TaricCode] = {
     val stream = new URL(url).openStream
-    parseFromGzipSource(stream)
+    decryptPgp(stream)
   }
 
-  def parseFromGzipSource(stream:InputStream) = {
+  private def decryptPgp(stream:InputStream) = {
+    import java.security.Security
+    import org.bouncycastle.jce.provider.BouncyCastleProvider
+    import org.bouncycastle.openpgp.PGPUtil
+
+    Security.addProvider(new BouncyCastleProvider())
+    val armored = PGPUtil.getDecoderStream(stream)
+    val pgpF = new PGPObjectFactory(armored)
+    val compressed = pgpF.nextObject().asInstanceOf[PGPCompressedData]
+    val pgpF2 = new PGPObjectFactory(compressed.getDataStream())
+    pgpF2.nextObject() // Throw away signature in this version. We do not verify it.
+    val literal = pgpF2.nextObject().asInstanceOf[PGPLiteralData]
+
+    parseFromGzipSource(literal.getInputStream)
+  }
+
+  private def parseFromGzipSource(stream:InputStream) = {
     val unzipped = new GZIPInputStream(stream)
     val source = Resource.fromInputStream(unzipped)
     parseFromPlainSource(source)
   }
 
-  def parseFromPlainSource(source:InputStreamResource[InputStream]) = for {
+  private def prodcode(line:String) = line.drop(3).take(10).trim
+  private def startDate(line:String) = line.drop(13).take(8).trim
+  private def endDate(line:String) = {
+    val tmp = line.drop(21).take(8).trim
+    if(tmp.length > 0) Option(tmp)
+    else None
+  }
+
+  private def parseFromPlainSource(source:InputStreamResource[InputStream]) = for {
     line <- source lines() drop(1)
   } yield TaricCode(prodcode(line), startDate(line), endDate(line))
 }
@@ -74,11 +97,33 @@ object TaricProdCodeParser {
  * Time: 21:13
  */
 object Taric {
+  // TODO 2012-12-03 Magnus: Do not use hardcoded URL:s. Check for latest KA-file and compare with current version.
   val url = "http://distr.tullverket.se/distr/taric/testflt/tot/3006_KA.tot.gz"
+  val url2 = "http://distr.tullverket.se/distr/taric/flt/tot/3030_KA.tot.gz.pgp"
 
-  def persist(codes:LongTraversable[ProductCode]) = {
+  //TODO 2012-12-03 Magnus: Handle KI (replaced) and KJ (added) code files
+
+  def persistFromTifUrl(url:String) {
+    persist(TaricProdCodeParser.parseFromURL(url))
+  }
+
+  def persist(codes:LongTraversable[TaricCode]) = {
+    // TODO 2012-12-03 Magnus: Use Oracle DB-driver instead.
     Database.forURL("jdbc:h2:mem:test1", driver = "org.h2.Driver") withSession {
+      (ProductCodes.ddl).create
 
+      for(c <- codes) {
+        ProductCodes.insertProjection.insert(c.hs, c.hsSub, c.cn, c.pres, !c.endDate.isDefined, "TARIC", c.startDate)
+      }
+
+      println("Select all that starts with 03")
+
+      val listOf03 = for {
+        pc <- ProductCodes
+        if( pc.hs.startsWith("03") )
+      } yield pc.id ~ pc.hs ~ pc.hsSub ~ pc.cn ~ pc.pres
+
+      for(pc <- listOf03) println(pc)
     }
   }
 }
--- project/Build.scala
+++ project/Build.scala
@@ -8,9 +8,9 @@ object ApplicationBuild extends Build {
     val appVersion      = "1.0.0-SNAPSHOT"
 
     val appDependencies = Seq(
-      //"org.bouncycastle" % "bcprov-jdk15on" % "1.47",
-      //"org.bouncycastle" % "bcpkix-jdk15on" % "1.47",
-      //"org.bouncycastle" % "bcpg-jdk15on" % "1.47",
+      "org.bouncycastle" % "bcprov-jdk15on" % "1.47",
+      "org.bouncycastle" % "bcpkix-jdk15on" % "1.47",
+      "org.bouncycastle" % "bcpg-jdk15on" % "1.47",
       "com.h2database" % "h2" % "1.3.168",
       "org.scalaquery" % "scalaquery_2.9.0-1" % "0.9.5",
       "org.scalaj" %% "scalaj-time" % "0.6",
